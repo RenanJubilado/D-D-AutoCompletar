7af6c8ab61a8228d5a8c86c1394f97eb250cde3b
Renan Jubilado <Renan.Soares@globalweb.com.br>
2018-01-31 17:32:32 -0200

    bootstrap
    Inicio do programa

---
+++ FichaFacil/src/app/menu/menu.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    this.atributos.forEach(element => {
+      element.modificador = 0;
+    });
+  }
+  
+  atributos: any = [
+    {atributo: 'FOR', valor: 10},
+    {atributo: 'DEX', valor: 10},
+    {atributo: 'CON', valor: 10},
+    {atributo: 'INT', valor: 10},
+    {atributo: 'SAB', valor: 10},
+    {atributo: 'CAR', valor: 10}
+  ];
+  
+  modCalc(atr: any){
+    this.atributos.forEach(element => {
+      if(atr.atributo == element.atributo)
+      element.modificador = Math.floor((atr.valor - 10)/2);
+    });
+  };
+
+}
